6bd3c64385bddb15b5fac9b7dd5b0f04d6ae201f
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-24 17:59:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
+  rooms = []
   names.each_with_index{|name,i| rooms << "Hello, #{name}! You'll be assigned to room #{i+1}!"}
   rooms
 end
